0c2b946ba7793564d83e0dacf65295a2b4a90da0
Abhishek <geekcv2022@gmail.com>
2024-02-17 14:50:56 +0530

    Triangle and Pyamid
    all pettern of Triangle and Pyamid

---
+++ Decreasing_Triangle.java
@@ -0,0 +1,12 @@
+public class Decreasing_Triangle {
+    public static void main(String[] args) {
+
+        for (int row = 1; row <= 5; row++) {
+            for (int col = row; col <= 5; col++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+}
